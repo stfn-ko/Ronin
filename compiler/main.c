1f355bffdf0f03e222cb750892c506f0aa483543
stfn_ko <stepan.kolontaj@gmail.com>
2023-08-10 19:46:13 +0100

    reversed a linked list

--- compiler/main.c
+++ compiler/main.c
@@ -4,28 +4,20 @@
 #include <string.h>
 
 /* TYPE DEFENITIONS */
-typedef signed char *isize;
-typedef long long i64;
-typedef long i32;
-typedef int i16;
-typedef char i8;
-
-typedef unsigned char *usize;
-typedef unsigned long long u64;
-typedef unsigned long u32;
-typedef unsigned int u16;
-typedef unsigned char u8;
-
+typedef long long integer_t;
 
 /* FILE HANDLING */
-long file_size(FILE *file) {
-    if (!file) {
+long file_size(FILE *file)
+{
+    if (!file)
+    {
         return 0;
     }
 
     fpos_t original = 0;
 
-    if (fgetpos(file, &original)) {
+    if (fgetpos(file, &original))
+    {
         printf("fgetpos() filed %i", errno);
         return 0;
     }
@@ -33,16 +25,19 @@ long file_size(FILE *file) {
     fseek(file, 0, SEEK_END);
     long out = ftell(file);
 
-    if (fsetpos(file, &original)) {
+    if (fsetpos(file, &original))
+    {
         printf("fsetpos() failed %i\n", errno);
     }
 
     return out;
 }
 
-char *file_contents(char *path) {
+char *file_contents(char *path)
+{
     FILE *file = fopen(path, "r");
-    if (!file) {
+    if (!file)
+    {
         printf("Could not open file at %s\n", path);
         return NULL;
     }
@@ -53,9 +48,11 @@ char *file_contents(char *path) {
     size_t bytes_read = 0;
     size_t remaining_bytes = size;
 
-    if (remaining_bytes > 0) {
+    if (remaining_bytes > 0)
+    {
         size_t brti = fread(write_it, 1, remaining_bytes, file);
-        if (ferror(file)) {
+        if (ferror(file))
+        {
             printf("Error while reading: %i\n", errno);
             free(contents);
             return NULL;
@@ -69,21 +66,23 @@ char *file_contents(char *path) {
     return contents;
 }
 
-
-void print_usage(char **argv) {
+void print_usage(char **argv)
+{
     printf("USAGE: %s <path_to_file_to_compile>", argv[0]);
 }
 
 /* ERRORS */
-#define ERROR_CREATE(n, t, msg_c)   \
+#define ERROR_CREATE(n, t, msg_c) \
     Error(n) = {(t), (msg_c)}
 
-#define ERROR_PREP(n, t, msg_p)     \
-    (n).type = (t);                 \
+#define ERROR_PREP(n, t, msg_p) \
+    (n).type = (t);             \
     (n).msg = (msg_p);
 
-typedef struct Error {
-    enum ErrorType {
+typedef struct Error
+{
+    enum ErrorType
+    {
         ERROR_NONE = 0,
         ERROR_ARGUMENTS,
         ERROR_TYPE,
@@ -97,14 +96,17 @@ typedef struct Error {
 
 Error ok = {ERROR_NONE, NULL};
 
-void print_error(Error err) {
-    if (err.type == ERROR_NONE) {
+void print_error(Error err)
+{
+    if (err.type == ERROR_NONE)
+    {
         return;
     }
     assert(ERROR_MAX == 6);
     printf("ERROR: ");
 
-    switch (err.type) {
+    switch (err.type)
+    {
     default:
         printf("Unknown error type...");
         break;
@@ -124,31 +126,34 @@ void print_error(Error err) {
     case ERROR_GENERIC:
         break;
     }
-    if (err.msg) {
+    if (err.msg)
+    {
         printf("\n\t-> %s", err.msg);
     }
 }
-
 
 /* LEXING */
 #define nonep(node) ((node).type == NODE_TYPE_NONE)
 #define integerp(node) ((node).type == NODE_TYPE_INTEGER)
 
 const char *whitespace = " \r\n";
-const char *delimiters = " \r\n,;/{}()<>"; 
+const char *delimiters = " \r\n,;/{}()<>";
 
 // TODO:
 // |-----API to create new node
 // |-----API to add node as child
-typedef struct Node {
-    enum NodeType {
+typedef struct Node
+{
+    enum NodeType
+    {
         NODE_TYPE_NONE,
         NODE_TYPE_INTEGER,
         NODE_TYPE_PROGRAM,
         NODE_TYPE_MAX,
     } type;
-    union NodeValue {
-        i32 integer;
+    union NodeValue
+    {
+        integer_t integer;
     } value;
     struct Node **children;
 } Node;
@@ -156,13 +161,15 @@ typedef struct Node {
 // TODO:
 // |-----API to create new Binding
 // |-----API to add Binding to Scope
-typedef struct Binding {
+typedef struct Binding
+{
     char *id;
     Node *value;
     struct Binding *next;
 } Binding;
 
-typedef struct Scope {
+typedef struct Scope
+{
     struct Scope *parent;
     Binding *bind;
 } Scope;
@@ -174,50 +181,68 @@ typedef struct Token
     struct Token *next;
 } Token;
 
-int token_string_eq(char *string, Token *token) {
-    if(!string||!token->beg||!token->end) return 0;
+int token_string_eq(char *string, Token *token)
+{
+    if (!string || !token->beg || !token->end)
+        return 0;
     char *beg = token->beg;
-    while (*string && token->beg < token->end) {
-        if (*string != *beg) return 0;
+    while (*string && token->beg < token->end)
+    {
+        if (*string != *beg)
+            return 0;
         string++;
         beg++;
     }
     return 1;
 }
 
-Token *token_create() {
+Token *token_create()
+{
     Token *token = malloc(sizeof(Token));
     assert(token && "Could not allocate memory token");
-    memset(token, 0, sizeof(Token)); 
+    memset(token, 0, sizeof(Token));
     return token;
 }
 
-void free_tokens(Token *root) {
-    while (root) {
+void free_tokens(Token *root)
+{
+    while (root)
+    {
         Token *token_to_free = root;
         root = root->next;
         free(token_to_free);
     }
 }
 
-void print_tokens(Token *root) {
+void print_tokens(Token *root)
+{
     size_t count = 1;
-    while (root) {
-        if (count > 10000) break; //FIXME
-        printf("Token %zu: ", count);
-        if (root->beg && root->end) {
-            printf("%.*s", root->end - root->beg, root->beg);
+    while (root)
+    {
+        if (count > 10000)
+        {
+            break; // FIXME {remove this limit}
         }
-        putchar('\n');
-        root = root->next; 
+
+        printf("Token %zu: ", count);
+
+        if (root->beg && root->end)
+        {
+            printf("%.*s\n", root->end - root->beg, root->beg);
+        }
+
+        root = root->next;
+
         count++;
     }
 }
 
 /* given a src (souce), get the next token, and point to it with beg (begin) & end (end)*/
-Error lex(char *src, Token *token) {
+Error lex(char *src, Token *token)
+{
     Error err = ok;
-    if (!src || !token) {
+    if (!src || !token)
+    {
         ERROR_PREP(err, ERROR_ARGUMENTS, "Can't lex empty source");
         return err;
     };
@@ -225,76 +250,94 @@ Error lex(char *src, Token *token) {
     token->beg = src;
     token->beg += strspn(token->beg, whitespace);
     token->end = token->beg;
-    
-    if (*(token->end) == '\0') {return err;}
+
+    if (*(token->end) == '\0')
+    {
+        return err;
+    }
     token->end += strcspn(token->beg, delimiters);
-    if (token->end == token->beg) {token->end += 1;}
+    if (token->end == token->beg)
+    {
+        token->end += 1;
+    }
     return err;
 }
 
-void scope_set() {
+void scope_set()
+{
 }
 
-Error parse_expr(char *src, Node *res) {
+Error parse_expr(char *src, Node *res)
+{
     Token *tokens = NULL;
-    Token *token_iter = tokens;
+    Token *tokens_it = tokens;
     Token current_token;
     current_token.beg = src;
     current_token.end = src;
     current_token.next = NULL;
-    
+
     Error err = ok;
 
-    while ((err = lex(current_token.end, &current_token)).type == ERROR_NONE) {
-        if (current_token.end - current_token.beg == 0) {
+    while ((err = lex(current_token.end, &current_token)).type == ERROR_NONE)
+    {
+        if (current_token.end - current_token.beg == 0)
+        {
             break;
         }
 
-        if(tokens) {
-            token_iter->next = token_create();
-            memcpy(token_iter->next, &current_token, sizeof(Token));
-            token_iter = token_iter->next;
-        } else {
+        if (tokens)
+        {
+            tokens_it->next = token_create();
+            memcpy(tokens_it->next, &current_token, sizeof(Token));
+            tokens_it = tokens_it->next;
+        }
+        else
+        {
             tokens = token_create();
             memcpy(tokens, &current_token, sizeof(Token));
-            token_iter = tokens;
+            tokens_it = tokens;
         }
     }
 
     print_tokens(tokens);
-    
+
     Node *root = calloc(1, sizeof(Node));
     assert(root && "Could not allocate memory for AST Node");
-    
-    token_iter = tokens;
 
-    while(token_iter) {
+    tokens_it = tokens;
+
+    while (tokens_it)
+    {
         // TODO: map constructs from the lanf and attempt to create nodes
-        size_t token_length = token_iter->end - token_iter->beg;
+        size_t token_length = tokens_it->end - tokens_it->beg;
         char *token_contents = malloc(token_length + 1);
         assert(token_contents && "Could not allocate string for token contents while parsing");
-        memcpy(token_contents, token_iter->beg, token_length);
+        memcpy(token_contents, tokens_it->beg, token_length);
         token_contents[token_length] = '\0';
 
-        if (token_string_eq("i32", token_iter)) {
+        if (token_string_eq("i32", tokens_it))
+        {
             printf("Found 'i32' at token\n");
 
-            if (token_string_eq("/", token_iter->next)) {
-                if (token_string_eq("rw", token_iter->next->next)) {
+            if (token_string_eq("/", tokens_it->next))
+            {
+                if (token_string_eq("rw", tokens_it->next->next))
+                {
                     printf("Found 'rw' at token\n");
                 }
-                else if (token_string_eq("r", token_iter->next->next)) {
+                else if (token_string_eq("r", tokens_it->next->next))
+                {
                     printf("Found 'r' at token\n");
                 }
             }
-            
-            if (token_iter->next && token_string_eq("=", token_iter->next->next)) {
-                printf("Found assignment\n");
-            } 
-            
+
+            if (tokens_it->next && token_string_eq("=", tokens_it->next->next))
+            {
+                printf("Found assignment operator '='\n");
+            }
         }
 
-        token_iter = token_iter->next;
+        tokens_it = tokens_it->next;
     }
 
     free_tokens(tokens);
@@ -303,8 +346,10 @@ Error parse_expr(char *src, Node *res) {
 }
 
 /* MAIN */
-int main(int argc, char **argv) {
-    if (argc < 2) {
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
         print_usage(argv);
         exit(0);
     }
@@ -312,7 +357,8 @@ int main(int argc, char **argv) {
     char *path = argv[1];
     char *contents = file_contents(path);
 
-    if (contents) {
+    if (contents)
+    {
         // printf("Contents of %s:\n---\n\"%s\"\n---\n", path, contents);
         Node *expression;
         print_error(parse_expr(contents, expression));
