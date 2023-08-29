4a6952f3a7d3649295024cdd256fc801b41f6bcb
stfn_ko <stepan.kolontaj@gmail.com>
2023-08-29 14:13:15 +0100

    beggining of lexing (skipping ws)

--- compiler/main.c
+++ compiler/main.c
@@ -1,18 +1,22 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "ANSI.h"
 
-enum tok_type {
-    tok_keyword, 
-    tok_operator,
+enum tok_type
+{
+    tok_keyword,
+    tok_identifier,
     tok_literal,
-    tok_identifier
+    tok_punctuation,
+    tok_delimeter
 };
 
-typedef struct token {
+typedef struct token
+{
     enum tok_type type;
     char *lexeme;
-    long line;
+    int line;
     int col;
 } token;
 
@@ -20,11 +24,17 @@ char *readf_2buff(char *_fpath)
 {
     if (!_fpath)
     {
-        printf(RC_Red "\n\t --> error: " Reset "no input files\n\n");
+        printf(RC_Red "\n\t --> error: " Reset "no input files.\n\n");
         exit(EXIT_FAILURE);
     }
 
     FILE *fp = fopen(_fpath, "rb");
+
+    if (!fp)
+    {
+        printf(RC_Red "\n\t --> error: " Reset "no such file or directory.\n\n");
+        exit(EXIT_FAILURE);
+    }
 
     // get file size
     fseek(fp, 0, SEEK_END);
@@ -35,13 +45,13 @@ char *readf_2buff(char *_fpath)
 
     if (!buff)
     {
-        printf(RC_Red "\n\t --> error: " Reset "out of memory\n\n");
+        printf(RC_Red "\n\t --> error: " Reset "out of memory.\n\n");
         exit(EXIT_FAILURE);
     }
 
     if (fread(buff, 1, fs, fp) != fs)
     {
-        printf(RC_Red "\n\t --> error: " Reset "eof reached while reading\n\n");
+        printf(RC_Red "\n\t --> error: " Reset "eof reached while reading.\n\n");
         exit(EXIT_FAILURE);
     }
 
@@ -51,9 +61,30 @@ char *readf_2buff(char *_fpath)
     return buff;
 }
 
-void lex(const char *src)
+static const char *whitespace = " \r\n";
+static const char *delimiters = "(){}[]";
+static const char *punctuators = "+-/*=,.;";
+
+void *lex(char *_src)
 {
-       
+    char *beg = _src;
+    char *end = _src;
+
+    while (*end != '\0')
+    {
+        if (!_src || !beg || !end)
+        {
+            printf(RC_Red "\n\t --> error: " Reset "can't lex an empty source.\n\n");
+            exit(EXIT_FAILURE);
+        }
+
+        end += strcspn(beg, whitespace);
+        if (end - beg)
+            printf("lexed: %.*s\n", end - beg, beg);
+        else
+            end = ++beg;
+        beg = end;
+    }
 }
 
 void parse_expr(char *_fpath)
@@ -64,7 +95,6 @@ void parse_expr(char *_fpath)
 
 int main(int argc, char **argv)
 {
-    // printf("%s", parsef(argv[1]));
     parse_expr(argv[1]);
     return 0;
 }
