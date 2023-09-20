2b9f2991b5917e7a416acf115c7e86b71bff2e01
stfn_ko <stepan.kolontaj@gmail.com>
2023-09-20 19:26:50 +0100

    func name & args update (perroex->err_ex_p)

--- compiler/file_handler.c
+++ compiler/file_handler.c
@@ -2,16 +2,18 @@
 #include <stdio.h>
 #include "error_handler.c"
 
+#define FL __FILE__,__LINE__
+
 // ===-------------------------------------------=== readf_2buff
 char *readf_2buff(char *_fpath)
 {
-    if(!_fpath) perroex("no input files");
+    if(!_fpath) err_ex_p("no input files", FL);
 
     FILE *fp = fopen(_fpath, "rb");
 
-    if(!fp) perroex("no such file or directory");
+    if(!fp) err_ex_p("no such file or directory", FL);
 
-    if (!strstr(_fpath, ".ro"))  perroex("invalid file extension");
+    if (!strstr(_fpath, ".ro"))  err_ex_p("invalid file extension", FL);
 
     // get file size
     fseek(fp, 0, SEEK_END);
@@ -20,9 +22,9 @@ char *readf_2buff(char *_fpath)
 
     char *buff = (char *)malloc(sizeof(char) * (fs + 1));
 
-    if(!buff) perroex("out of memory");
+    if(!buff) err_ex_p("couldn't allocate memory for text buffer", FL);
 
-    if(fread(buff, 1, fs, fp) != fs) perroex("eof reached while reading file");
+    if(fread(buff, 1, fs, fp) != fs) err_ex_p("eof reached while reading file", FL);
 
     fclose(fp);
     buff[fs] = '\0';
