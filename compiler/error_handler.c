508bedf0dec3236f97baf7d6e30291dfae155075
stfn_ko <stepan.kolontaj@gmail.com>
2023-10-14 20:51:16 +0100

    err_ex_p msg branching

--- compiler/error_handler.c
+++ compiler/error_handler.c
@@ -1,19 +1,26 @@
 // ===-------------------------------------------=== INCLUDES
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include "ANSI.h"
 
 // ===-------------------------------------------=== print error on exit
-void err_ex_p(char *_err_msg, const char *__FILE, size_t __LINE)
+void err_ex_p(const char *_err_msg, const char *__FILE, uint32_t __LINE)
 {
-    printf(UL_Red "\nERROR" Reset B_Red " ( %s | ln:%d ) >>> " Reset "%s\n\n",
-           __FILE, __LINE, _err_msg);
+    if (__LINE)
+    {
+        printf(UL_Red "\nERROR" Reset B_Red " ( %s | ln:%d ) >>> " Reset "%s\n\n", __FILE, __LINE, _err_msg);
+    }
+    else
+    {
+        printf(UL_Red "\nERROR" Reset B_Red " ( %s ) >>> " Reset "%s\n\n", __FILE, _err_msg);
+    }
 
     exit(EXIT_FAILURE);
 }
 
 // ===-------------------------------------------=== DEBUG
-void todo_err(const char *_msg, const char *__FILE, size_t __LINE)
+void todo_err(const char *_msg, const char *__FILE, uint32_t __LINE)
 {
     printf(UL_Purple "\nTODO" Reset B_Purple " ( %s | ln:%d ) >>> " Reset "%s\n\n",
            __FILE, __LINE, _msg);
