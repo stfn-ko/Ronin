4f2f7bf3e4d470a19c414ec58f67986dae1cf2a4
stfn_ko <stepan.kolontaj@gmail.com>
2023-10-17 13:26:52 +0100

    reorg

--- compiler/error_handler.c
+++ compiler/error_handler.c
@@ -1,8 +1,16 @@
-// ===-------------------------------------------=== INCLUDES
+#ifndef ERROR_HANDLER_C
+#define ERROR_HANDLER_C
+
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+
 #include "ANSI.h"
+
+#define EXIT_TODO 10
+#define EXIT_CRITICAL 11
+#define FL __FILE__, __LINE__
 
 // ===-------------------------------------------=== print error on exit
 void err_ex_p(const char *_err_msg, const char *__FILE, uint32_t __LINE)
@@ -16,7 +24,7 @@ void err_ex_p(const char *_err_msg, const char *__FILE, uint32_t __LINE)
         printf(UL_Red "\nERROR" Reset B_Red " ( %s ) >>> " Reset "%s\n\n", __FILE, _err_msg);
     }
 
-    exit(EXIT_FAILURE);
+    exit(EXIT_CRITICAL);
 }
 
 // ===-------------------------------------------=== DEBUG
@@ -25,5 +33,7 @@ void todo_err(const char *_msg, const char *__FILE, uint32_t __LINE)
     printf(UL_Purple "\nTODO" Reset B_Purple " ( %s | ln:%d ) >>> " Reset "%s\n\n",
            __FILE, __LINE, _msg);
 
-    exit(EXIT_FAILURE);
+    exit(EXIT_TODO);
 }
+
+#endif
