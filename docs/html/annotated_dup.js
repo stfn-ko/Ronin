16465c8c5e3295ca76208dcc0ea24ea8ec874cec
Stephan Kolontay <85121881+stfn-ko@users.noreply.github.com>
2023-10-17 13:44:43 +0100

    Delete docs directory

--- docs/html/annotated_dup.js
+++
@@ -1,6 +0,0 @@
-var annotated_dup =
-[
-    [ "Lexeme", "struct_lexeme.html", null ],
-    [ "Scope", "struct_scope.html", null ],
-    [ "Token", "struct_token.html", null ]
-];
--- docs/html/files_dup.js
+++
@@ -1,7 +0,0 @@
-var files_dup =
-[
-    [ "ANSI.h", "_a_n_s_i_8h_source.html", null ],
-    [ "error_handler.c", "error__handler_8c_source.html", null ],
-    [ "file_handler.c", "file__handler_8c_source.html", null ],
-    [ "lexer.h", "lexer_8h_source.html", null ]
-];
