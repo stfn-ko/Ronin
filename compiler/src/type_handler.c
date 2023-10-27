f95548a6dd41e654bb055aaf43554b64a86a9242
stfn_ko <stepan.kolontaj@gmail.com>
2023-10-27 23:34:03 +0100

    implemented strlen, new methods of comp

--- compiler/src/type_handler.c
+++ compiler/src/type_handler.c
@@ -5,12 +5,19 @@
 /***********************************************************/
 
 token_t is_keyword(const char **_lxm);
-token_t is_unsigned_int(const char **_lxm);
-token_t is_signed_int(const char **_lxm);
-token_t is_float(const char **_lxm);
-token_t is_char(const char **_lxm);
-token_t is_bool(const char **_lxm);
-token_t is_str(const char **_lxm);
+token_t is_punctuation(const char **_lxm);
+token_t is_delimiter(const char **_lxm);
+
+token_t is_int_kw(const char **_lxm);
+token_t is_uint_kw(const char **_lxm);
+token_t is_float_kw(const char **_lxm);
+token_t is_bool_kw(const char **_lxm);
+token_t is_char_kw(const char **_lxm);
+token_t is_str_kw(const char **_lxm);
+token_t is_perm_kw(const char **_lxm);
+token_t is_return_kw(const char **_lxm);
+token_t is_loop_kw(const char **_lxm);
+token_t is_ctrl_flow_kw(const char **_lxm);
 
 /***********************************************************/
 /*                    PUBLIC FUNCTIONS                     */
@@ -31,11 +38,98 @@ size_t str_cmp(const char *_str1, const char *_str2)
     return 1;
 }
 
+// https://elixir.bootlin.com/glibc/glibc-2.17/source/string/strlen.c
+size_t str_len(const char *_str)
+{
+    const char *char_ptr;
+    const unsigned long int *longword_ptr;
+    unsigned long int longword, himagic, lomagic;
+
+    for (char_ptr = _str; ((unsigned long int)char_ptr & (sizeof(longword) - 1)) != 0;
+         ++char_ptr)
+        if (*char_ptr == '\0')
+            return char_ptr - _str;
+
+    longword_ptr = (unsigned long int *)char_ptr;
+    himagic = 0x80808080L;
+    lomagic = 0x01010101L;
+
+    if (sizeof(longword) > 4)
+    {
+        himagic = ((himagic << 16) << 16) | himagic;
+        lomagic = ((lomagic << 16) << 16) | lomagic;
+    }
+
+    if (sizeof(longword) > 8)
+    {
+        abort();
+    }
+
+    for (;;)
+    {
+        longword = *longword_ptr++;
+
+        if (((longword - lomagic) & ~longword & himagic) != 0)
+        {
+            const char *cp = (const char *)(longword_ptr - 1);
+
+            if (cp[0] == 0)
+            {
+                return cp - _str;
+            }
+            if (cp[1] == 0)
+            {
+                return cp - _str + 1;
+            }
+            if (cp[2] == 0)
+            {
+                return cp - _str + 2;
+            }
+            if (cp[3] == 0)
+            {
+                return cp - _str + 3;
+            }
+            if (sizeof(longword) > 4)
+            {
+                if (cp[4] == 0)
+                {
+                    return cp - _str + 4;
+                }
+                if (cp[5] == 0)
+                {
+                    return cp - _str + 5;
+                }
+                if (cp[6] == 0)
+                {
+                    return cp - _str + 6;
+                }
+                if (cp[7] == 0)
+                {
+                    return cp - _str + 7;
+                }
+            }
+        }
+    }
+}
+
 token_t deduce_tok_type(const char **_lxm)
 {
     size_t res = undefined;
 
-    res = is_keyword(_lxm);
+    if (str_len(*_lxm) > 1)
+    {
+        res = is_keyword(_lxm);
+    }
+
+    if (res == undefined)
+    {
+        res = is_punctuation(_lxm);
+    }
+
+    if (res == undefined)
+    {
+        res = is_delimiter(_lxm);
+    }
 
     return res;
 }
@@ -44,7 +138,7 @@ token_t deduce_tok_type(const char **_lxm)
 /*                  FUNCTION DECLARATIONS                  */
 /***********************************************************/
 
-token_t is_signed_int(const char **_lxm)
+token_t is_int_kw(const char **_lxm)
 {
     if (**_lxm == 'i')
     {
@@ -73,7 +167,7 @@ token_t is_signed_int(const char **_lxm)
     return undefined;
 }
 
-token_t is_unsigned_int(const char **_lxm)
+token_t is_uint_kw(const char **_lxm)
 {
     if (**_lxm == 'i')
     {
@@ -106,7 +200,7 @@ token_t is_unsigned_int(const char **_lxm)
     return undefined;
 }
 
-token_t is_float(const char **_lxm)
+token_t is_float_kw(const char **_lxm)
 {
     if (**_lxm == 'f')
     {
@@ -126,17 +220,25 @@ token_t is_float(const char **_lxm)
 
     return undefined;
 }
-token_t is_bool(const char **_lxm)
+token_t is_bool_kw(const char **_lxm)
 {
     if (str_cmp(*_lxm, "bool"))
     {
         return KW_BOOL;
     }
+    else if (str_cmp(*_lxm, "true"))
+    {
+        return KW_TRUE;
+    }
+    else if (str_cmp(*_lxm, "false"))
+    {
+        return KW_FALSE;
+    }
 
     return undefined;
 }
 
-token_t is_char(const char **_lxm)
+token_t is_char_kw(const char **_lxm)
 {
     if (str_cmp(*_lxm, "char"))
     {
@@ -146,7 +248,7 @@ token_t is_char(const char **_lxm)
     return undefined;
 }
 
-token_t is_str(const char **_lxm)
+token_t is_str_kw(const char **_lxm)
 {
     if (str_cmp(*_lxm, "str"))
     {
@@ -156,11 +258,110 @@ token_t is_str(const char **_lxm)
     return undefined;
 }
 
+token_t is_perm_kw(const char **_lxm)
+{
+    if (**_lxm == 'r')
+    {
+        if (str_cmp(*_lxm, "r"))
+        {
+            return KW_READ_ONLY;
+        }
+        else if (str_cmp(*_lxm, "rx"))
+        {
+            return KW_READ_ONLY_UNIQUE;
+        }
+        else if (str_cmp(*_lxm, "rs"))
+        {
+            return KW_READ_ONLY_SHARED;
+        }
+        else if (str_cmp(*_lxm, "ro"))
+        {
+            return KW_READ_ONLY_OBSERVER;
+        }
+        else if (str_cmp(*_lxm, "rw"))
+        {
+            return KW_READ_AND_WIRTE;
+        }
+        else if (str_cmp(*_lxm, "rwx"))
+        {
+            return KW_READ_AND_WIRTE_UNIQUE;
+        }
+        else if (str_cmp(*_lxm, "rwo"))
+        {
+            return KW_READ_AND_WIRTE_OBSERVER;
+        }
+    }
+
+    return undefined;
+}
+
+token_t is_return_kw(const char **_lxm)
+{
+    if (str_cmp(*_lxm, "return"))
+    {
+        return KW_RETURN;
+    }
+
+    return undefined;
+}
+
+token_t is_loop_kw(const char **_lxm)
+{
+    if (str_cmp(*_lxm, "while"))
+    {
+        return KW_WHILE;
+    }
+    else if (str_cmp(*_lxm, "for"))
+    {
+        return KW_FOR;
+    }
+    else if (str_cmp(*_lxm, "break"))
+    {
+        return KW_BREAK;
+    }
+    else if (str_cmp(*_lxm, "skip"))
+    {
+        return KW_SKIP;
+    }
+
+    return undefined;
+}
+
+token_t is_ctrl_flow_kw(const char **_lxm)
+{
+    if (str_cmp(*_lxm, "if"))
+    {
+        return KW_IF;
+    }
+    else if (str_cmp(*_lxm, "else"))
+    {
+        return KW_ELSE;
+    }
+    else if (str_cmp(*_lxm, "match"))
+    {
+        return KW_MATCH;
+    }
+
+    return undefined;
+}
+
 token_t is_keyword(const char **_lxm)
 {
     token_t res = undefined;
 
-    token_t (*keyword_checks[])(const char **) = {is_signed_int, is_unsigned_int, is_float, is_bool, is_char, is_str};
+    token_t (*keyword_checks[])(const char **) =
+        {
+            is_int_kw,
+            is_uint_kw,
+            is_float_kw,
+            is_bool_kw,
+            is_char_kw,
+            is_str_kw,
+            is_perm_kw,
+            is_return_kw,
+            is_loop_kw,
+            is_ctrl_flow_kw};
+
     size_t arr_size = sizeof(keyword_checks) / sizeof(keyword_checks[0]);
 
     for (size_t i = 0; i < arr_size; ++i)
@@ -173,4 +374,112 @@ token_t is_keyword(const char **_lxm)
     }
 
     return res;
+}
+
+token_t is_punctuation(const char **_lxm)
+{
+    if (str_cmp(*_lxm, "+"))
+    {
+        return PUNC_PLUS;
+    }
+    else if (str_cmp(*_lxm, "-"))
+    {
+        return PUNC_MINUS;
+    }
+    else if (str_cmp(*_lxm, "/"))
+    {
+        return PUNC_FW_SLASH;
+    }
+    else if (str_cmp(*_lxm, "*"))
+    {
+        return PUNC_STAR;
+    }
+    else if (str_cmp(*_lxm, "%"))
+    {
+        return PUNC_PERCENT;
+    }
+    else if (str_cmp(*_lxm, "^"))
+    {
+        return PUNC_CARET;
+    }
+    else if (str_cmp(*_lxm, "!"))
+    {
+        return PUNC_NOT;
+    }
+    else if (str_cmp(*_lxm, "&"))
+    {
+        return PUNC_AND;
+    }
+    else if (str_cmp(*_lxm, "|"))
+    {
+        return PUNC_OR;
+    }
+    else if (str_cmp(*_lxm, "="))
+    {
+        return PUNC_EQ;
+    }
+    else if (str_cmp(*_lxm, ">"))
+    {
+        return PUNC_GT;
+    }
+    else if (str_cmp(*_lxm, "<"))
+    {
+        return PUNC_LT;
+    }
+    else if (str_cmp(*_lxm, "@"))
+    {
+        return PUNC_AT;
+    }
+    else if (str_cmp(*_lxm, "#"))
+    {
+        return PUNC_HTAG;
+    }
+    else if (str_cmp(*_lxm, ","))
+    {
+        return PUNC_COMMA;
+    }
+    else if (str_cmp(*_lxm, "."))
+    {
+        return PUNC_DOT;
+    }
+    else if (str_cmp(*_lxm, ";"))
+    {
+        return PUNC_SEMI;
+    }
+    else if (str_cmp(*_lxm, ":"))
+    {
+        return PUNC_COL;
+    }
+
+    return undefined;
+}
+
+token_t is_delimiter(const char **_lxm)
+{
+    if (str_cmp(*_lxm, "("))
+    {
+        return DELIM_BRACE_L;
+    }
+    else if (str_cmp(*_lxm, ")"))
+    {
+        return DELIM_BRACE_R;
+    }
+    else if (str_cmp(*_lxm, "["))
+    {
+        return DELIM_SQ_BRACE_L;
+    }
+    else if (str_cmp(*_lxm, "]"))
+    {
+        return DELIM_SQ_BRACE_R;
+    }
+    else if (str_cmp(*_lxm, "{"))
+    {
+        return DELIM_SQ_BRACE_L;
+    }
+    else if (str_cmp(*_lxm, "}"))
+    {
+        return DELIM_SQ_BRACE_R;
+    }
+
+    return undefined;
 }
