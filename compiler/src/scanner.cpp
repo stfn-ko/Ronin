81157ee0c7e333c490ca3755a409333d6b475e71
stfn_ko <stepan.kolontaj@gmail.com>
2024-02-12 00:21:44 +0200

    removed comments, renamed funcs, did some wizardy

--- compiler/src/scanner.cpp
+++ compiler/src/scanner.cpp
@@ -18,12 +18,15 @@ std::unordered_map<std::string, token_t> keyword_map =
         {"str", KW_TYPE_STR},
 
         {"fn", KW_FN},
+        {"mod", KW_MOD},
+
         {"if", KW_IF},
         {"else", KW_ELSE},
         {"for", KW_FOR},
         {"while", KW_WHILE},
         {"return", KW_RETURN},
 
+        {"::", COMB_ACCESS},
         {">=", COMB_GTOE},
         {"<=", COMB_LTOE},
         {"==", COMB_EQ},
@@ -62,158 +65,10 @@ std::unordered_map<std::string, token_t> keyword_map =
         {",", MISC_COMMA},
 };
 
-// void skip_comments(auto &it, auto &pos, auto end)
-// {
-//     auto start = it++;
-//     while (it != end && *it != '\n')
-//     {
-//         ++it;
-//         ++pos.col;
-//     }
-// }
-
-// void skip_whitespace(auto &it, auto &pos)
-// {
-//     if (*it == '\n')
-//     {
-//         pos.col = 1;
-//         ++pos.ln;
-//     }
-//     else
-//     {
-//         ++pos.col;
-//     }
-
-//     ++it;
-// }
-
 auto is_digits(const std::string &str) -> bool
 {
     return str.find_first_not_of("0123456789") == std::string::npos;
 }
-
-// auto is_delim(auto &it) -> bool
-// {
-//     return (std::isspace(*it) || std::ispunct(*it)) && *it != '_';
-// }
-
-// void read_string(auto &tokens, auto &start, auto &it, auto &pos, auto &end)
-// {
-//     while (*(++it) != '"')
-//     {
-//         error(it == end, "Unterminated string literal");
-//     }
-
-//     tokens.emplace_back(token{std::string(start, ++it), token_t::LIT_STR, pos});
-//     pos.col += it - start;
-// }
-
-// void read_keyword_with_delimeter(auto &start, auto &it)
-// {
-//     int iter = 4;
-//     auto tmp_str = std::string(start, it + iter);
-
-//     while (keyword_map.find(tmp_str) == keyword_map.end())
-//     {
-//         --iter;
-//         tmp_str.pop_back();
-//     }
-
-//     if (iter < 4)
-//     {
-//         it += iter;
-//     }
-// }
-
-// void get_tokens(auto &tokens, auto &it, auto &pos, auto end)
-// {
-//     auto start = it;
-
-//     if (*it == '_' || isalpha(*it))
-//     {
-//         // get identifiers
-//         while ((isalnum(*it) || *it == '_') && it != end)
-//         {
-//             ++it;
-//         }
-//     }
-
-//     else if (isdigit(*it))
-//     {
-//         auto punct_used = 0;
-//         // get number literal
-//         while ((isdigit(*it) || *it == '.') && it != end && punct_used <= 1)
-//         {
-//             if (*it == '.')
-//             {
-//                 punct_used++;
-//             }
-
-//             it++;
-//         }
-//     }
-
-//     if (start == it)
-//     {
-//         // read delimiter
-//         ++it;
-//     }
-
-//     if (*start == '"')
-//     {
-//         read_string(tokens, start, it, pos, end);
-//         return;
-//     }
-//     else if (std::string("+-=></").find(*start) != std::string::npos)
-//     {
-//         read_keyword_with_delimeter(start, it);
-//     }
-
-//     auto lxm = std::string(start, it);
-//     auto type = keyword_map.find(lxm);
-
-//     if (is_digits(lxm))
-//     {
-//         tokens.emplace_back(token{lxm, token_t::LIT_NUM, pos});
-//     }
-//     else if (type == keyword_map.end())
-//     {
-//         tokens.emplace_back(token{lxm, token_t::IDENTIFIER, pos});
-//     }
-//     else
-//     {
-//         tokens.emplace_back(token{lxm, type->second, pos});
-//     }
-
-//     pos.col += it - start;
-// }
-
-// auto scan(const std::string &path) -> std::vector<token>
-// {
-//     auto buffer = read_file(path);
-//     error(buffer->empty(), "Can't parse an empty source");
-
-//     std::vector<token> tokens = {};
-//     position pos = {.ln = 1, .col = 1};
-
-//     for (auto it = buffer->begin(); it != buffer->end();)
-//     {
-//         if (*it == '#')
-//         {
-//             skip_comments(it, pos, buffer->end());
-//         }
-//         else if (!isspace(*it))
-//         {
-//             get_tokens(tokens, it, pos, buffer->end());
-//         }
-//         else
-//         {
-//             skip_whitespace(it, pos);
-//         }
-//     }
-
-//     return tokens;
-// }
 
 struct file_reader
 {
@@ -226,7 +81,12 @@ auto get_next(file_reader &fr) -> int
     return fr.iter = getc(fr.self);
 }
 
-void sc_(file_reader &fr)
+auto get_type(std::string &str) -> token_t
+{
+    return keyword_map.count(str) ? keyword_map.at(str) : token_t::IDENTIFIER;
+}
+
+void skip_comments(file_reader &fr)
 {
     while (fr.iter != EOF && fr.iter != '\n')
     {
@@ -234,7 +94,7 @@ void sc_(file_reader &fr)
     }
 }
 
-void sw_(file_reader &fr, position &pos)
+void skip_whitespace(file_reader &fr, position &pos)
 {
     if (fr.iter == '\n')
     {
@@ -249,86 +109,7 @@ void sw_(file_reader &fr, position &pos)
     get_next(fr);
 }
 
-// void gt_(std::vector<token> &tokens, file_reader &fr, position &pos)
-// {
-//     auto str = std::string();
-
-//     if (fr.iter == '_' || isalpha(fr.iter))
-//     {
-//         // get identifiers
-//         while ((isalnum(fr.iter) || fr.iter == '_') && fr.iter != EOF)
-//         {
-//             str.push_back(fr.iter);
-//             get_next(fr);
-//         }
-//     }
-//     else if (isdigit(fr.iter))
-//     {
-//         auto punct_used = 0;
-//         // get number literal
-//         while ((isdigit(fr.iter) || fr.iter == '.') && fr.iter != EOF && punct_used <= 1)
-//         {
-//             if (fr.iter == '.')
-//             {
-//                 punct_used++;
-//             }
-
-//             str.push_back(get_next(fr));
-//         }
-//     }
-//     else
-//     {
-//         str.push_back(get_next(fr));
-
-//         if (str.front() == '"')
-//         {
-//             while (get_next(fr) != '"')
-//             {
-//                 error(fr.iter == EOF, "Unterminated string literal");
-//                 str.push_back(fr.iter);
-//                 ++pos.col;
-//             }
-
-//             str.push_back(fr.iter);
-//             tokens.emplace_back(token{str, token_t::LIT_STR, pos});
-//             pos.col += str.size();
-//             return;
-//         }
-//         else if (std::string("><=-+&|").find(str.front()) != std::string::npos)
-//         {
-//             auto backup_self = ftell(fr.self);
-//             auto backup_iter = fr.iter;
-
-//             str.push_back(get_next(fr));
-
-//             if (keyword_map.find(str) == keyword_map.end())
-//             {
-//                 fseek(fr.self, backup_self, SEEK_CUR);
-//                 fr.iter = backup_iter;
-//                 str.pop_back();
-//             }
-//         }
-//     }
-
-//     auto type = keyword_map.find(str);
-
-//     if (is_digits(str))
-//     {
-//         tokens.emplace_back(token{str, token_t::LIT_NUM, pos});
-//     }
-//     else if (type == keyword_map.end())
-//     {
-//         tokens.emplace_back(token{str, token_t::IDENTIFIER, pos});
-//     }
-//     else
-//     {
-//         tokens.emplace_back(token{str, type->second, pos});
-//     }
-
-//     pos.col += str.size();
-// }
-
-void gt_(std::vector<token> &tokens, file_reader &fr, position &pos)
+void get_token(std::vector<token> &tokens, file_reader &fr, position &pos)
 {
     auto str = std::string();
 
@@ -355,6 +136,10 @@ void gt_(std::vector<token> &tokens, file_reader &fr, position &pos)
             str.push_back(fr.iter);
             get_next(fr);
         }
+
+        tokens.emplace_back(token{str, token_t::LIT_NUM, pos});
+        pos.col += str.size();
+        return;
     }
     else if (fr.iter == '\"')
     {
@@ -370,6 +155,10 @@ void gt_(std::vector<token> &tokens, file_reader &fr, position &pos)
 
         str.push_back(fr.iter);
         get_next(fr);
+
+        tokens.emplace_back(token{str, token_t::LIT_STR, pos});
+        pos.col += str.size();
+        return;
     }
     else if (fr.iter == '/')
     {
@@ -393,13 +182,14 @@ void gt_(std::vector<token> &tokens, file_reader &fr, position &pos)
 
         return;
     }
+    // handle the fucking combo tokens
     else
     {
         str.push_back(fr.iter);
         get_next(fr);
     }
 
-    tokens.emplace_back(token{str, token_t::UNDEFINED, pos});
+    tokens.emplace_back(token{str, get_type(str), pos});
     pos.col += str.size();
     return;
 }
@@ -418,15 +208,15 @@ auto scan(const std::string &path) -> std::vector<token>
     {
         if (fr.iter == '#')
         {
-            sc_(fr);
+            skip_comments(fr);
         }
         else if (isspace(fr.iter))
         {
-            sw_(fr, pos);
+            skip_whitespace(fr, pos);
         }
         else
         {
-            gt_(tokens, fr, pos);
+            get_token(tokens, fr, pos);
         }
     }
 
